f3f98e247517d10e7e90c15d5f2262175371cb28
dvoretskii-dd <dvoretskii-dd@MOIT0109>
2024-04-22 15:57:17 +0700

    Добавьте файлы проекта.

---
+++ ZEWS/AddHotelRoom.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZEWS
+{
+    /// <summary>
+    /// Логика взаимодействия для AddRedactHotelRoom.xaml
+    /// </summary>
+    public partial class AddHotelRoom : Page
+    {
+        public AddHotelRoom()
+        {
+            InitializeComponent();
+        }
+        private void Description_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Description.Text == "Описание")
+            {
+                Login.Text = "";
+            }
+        }
+
+        private void Description_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Description.Text == "")
+            {
+                Description.Text = "Описание";
+            }
+        }
+
+        private void Login_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Login.Text == "Логин")
+            {
+                Login.Text = "";
+            }
+        }
+
+        private void Login_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Login.Text == "")
+            {
+                Login.Text = "Логин";
+            }
+        }
+    }
+}
---
+++ ZEWS/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace ZEWS
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loginTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+               if (loginTextBox.Text == "Логин")
+               {
+                loginTextBox.Text = "";
+               }
+            
+        }
+
+        private void loginTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (loginTextBox.Text == "")
+            {
+                loginTextBox.Text = "Логин";
+            }
+        }
+
+        private void passwordTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (passwordTextBox.Password == "Пароль")
+            {
+                
+                passwordTextBox.Password = "";
+            }
+        }
+
+        private void passwordTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (passwordTextBox.Password == "")
+            {
+
+                passwordTextBox.Password = "Пароль";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.PostAsync("http://api-all.ru/api/login",
+                        new StringContent(json, Encoding.UTF8, "application/json"));
+
+                    response.EnsureSuccessStatusCode(); // Гарантирует, что ответ успешный
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    // Парсим ответ в объект
+                    var responseObject = JsonConvert.DeserializeObject<dynamic>(responseBody);
+                    string token = responseObject.token;
+
+                    // Сохраняем токен в настройках приложения
+                    Token.token = token;
+                    Properties.Settings.Default.Token = token;
+                    Properties.Settings.Default.Save();
+
+                    // Переход на другую страницу или какая-то другая логика после успешной аутентификации
+                    FrameManager.MainFrame.Navigate(new MainPage(mainWindow));
+                }
+
+                catch (Exception)
+                {
+                    // Ошибка при неправильно введенных данных
+                    MessageBox.Show("Проверьте правильность введенных данных");
+                }
+            }
+        }
+    }
+}
